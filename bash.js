2be22227da6e6160b5db0c32985d6f5707320fd1
Land4 <land4@land.local>
2017-07-31 11:08:33 -0400

    v3

--- bash.js
+++ bash.js
@@ -1,15 +1,16 @@
 
+var command = require('command.js')
 process.stdout.write('prompt >');
 
 process.stdin.on('data', (data)  => {
 	var cmd = data.toString().trim();
 	var array = [];
 	if(cmd === 'pwd'){
-		process.stdout.write(process.cwd() + '\n');
+		command.pwd();
 	}
 	if(cmd === 'date'){
 		process.stdout.write(Date() + '\n');
 	}
 	process.stdout.write('You typed: ' + cmd);
 	process.stdout.write('\nprompt > ');
-});
+})
---
+++ command.js
@@ -0,0 +1,5 @@
+module.exports = {
+	pwd: ()  => '${proccess.cwd()} \n'
+	}
+
+
