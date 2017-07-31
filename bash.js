c2251fbcce59426bc5fd122075ff61f898b44464
Jackson Sui <kiro705@gmail.com>
2017-07-31 10:55:53 -0400

    pwd and date function

--- bash.js
+++ bash.js
@@ -3,10 +3,13 @@ process.stdout.write('prompt >');
 
 process.stdin.on('data', (data)  => {
 	var cmd = data.toString().trim();
+	var array = [];
+	if(cmd === 'pwd'){
+		process.stdout.write(process.cwd() + '\n');
+	}
+	if(cmd === 'date'){
+		process.stdout.write(Date() + '\n');
+	}
 	process.stdout.write('You typed: ' + cmd);
-for (i in process.argv) {
-	process.argv[i];
-
-}
 	process.stdout.write('\nprompt > ');
 });
